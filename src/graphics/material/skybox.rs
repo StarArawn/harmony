cd112c92886801d249c605fe01e55fe5ce977a06
John Mitchell <toasterthegamer@gmail.com>
2020-05-28 17:13:43 -0400

    Remove poll of device.

--- src/graphics/material/skybox.rs
+++ src/graphics/material/skybox.rs
@@ -108,8 +108,6 @@ impl Skybox {
                 self.size,
             );
 
-            device.poll(wgpu::Maintain::Wait);
-
             let color_view = color.texture.create_view(&wgpu::TextureViewDescriptor {
                 label: None,
                 format: wgpu::TextureFormat::Rgba32Float,
@@ -126,9 +124,9 @@ impl Skybox {
                 address_mode_u: wgpu::AddressMode::ClampToEdge,
                 address_mode_v: wgpu::AddressMode::ClampToEdge,
                 address_mode_w: wgpu::AddressMode::ClampToEdge,
-                mag_filter: wgpu::FilterMode::Linear,
-                min_filter: wgpu::FilterMode::Linear,
-                mipmap_filter: wgpu::FilterMode::Linear,
+                mag_filter: wgpu::FilterMode::Nearest,
+                min_filter: wgpu::FilterMode::Nearest,
+                mipmap_filter: wgpu::FilterMode::Nearest,
                 lod_min_clamp: -100.0,
                 lod_max_clamp: 100.0,
                 compare: wgpu::CompareFunction::Undefined,
