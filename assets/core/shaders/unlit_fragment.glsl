dcfa98dfe5f2a3f108946a9b94ea13db8f423144
StarToaster <startoaster23@gmail.com>
2020-05-29 10:33:01 -0400

    Fixed issues with rendering.

--- assets/core/shaders/unlit_fragment.glsl
+++ assets/core/shaders/unlit_fragment.glsl
@@ -6,10 +6,10 @@ layout(location = 0) out vec4 outColor;
 layout(set = 2, binding = 0) uniform Locals {
     vec3 color;
 };
-layout(set = 2, binding = 1) uniform texture2D t_Color;
-layout(set = 2, binding = 2) uniform sampler s_Color;
+// layout(set = 2, binding = 1) uniform texture2D t_Color;
+// layout(set = 2, binding = 2) uniform sampler s_Color;
 
 void main() {
-    vec4 tex = texture(sampler2D(t_Color, s_Color), v_TexCoord);
-    outColor = tex;
+    // vec4 tex = texture(sampler2D(t_Color, s_Color), v_TexCoord);
+    outColor = vec4(1, 0, 0, 1);
 }
