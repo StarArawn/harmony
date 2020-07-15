8132a52faa06494ebd6e1f5e03849acf3ec4e2f4
John Mitchell <toasterthegamer@gmail.com>
2020-07-15 08:45:05 -0400

    Cleaned up some older code.

--- src/graphics/material/mod.rs
+++ src/graphics/material/mod.rs
@@ -1,13 +1,2 @@
 pub(crate) mod skybox;
 pub use self::skybox::Skybox;
-
-// pub(crate) mod unlit_material;
-// pub use self::unlit_material::*;
-
-// pub(crate) mod pbr_material;
-// pub use self::pbr_material::*;
-
-// pub enum Material {
-//     Unlit(UnlitMaterial),
-//     PBR(PBRMaterial),
-// }
