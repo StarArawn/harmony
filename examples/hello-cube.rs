6985803207d14cd0339a9b23551ad78294846f9c
John Mitchell <toasterthegamer@gmail.com>
2020-07-15 22:22:28 -0400

    Revert the hello-cube changes.

--- examples/hello-cube.rs
+++ examples/hello-cube.rs
@@ -43,8 +43,8 @@ fn create_rotate_system() -> Box<dyn Schedulable> {
         .with_query(<Write<Transform>>::query())
         .build(|_, mut world, delta_time, transform_query| {
             for mut transform in transform_query.iter_mut(&mut world) {
-                // transform.rotate_on_y(-0.5 * delta_time.0);
-                // transform.rotate_on_x(-0.5 * delta_time.0);
+                transform.rotate_on_y(-0.5 * delta_time.0);
+                transform.rotate_on_x(-0.5 * delta_time.0);
             }
         })
 }
@@ -94,7 +94,7 @@ impl harmony::AppState for AppState {
             // Note: This could be loading still, but in our case we don't care as the system that renders the meshes
             // will wait until the mesh is finished loading before displaying it. If the loading fails you wont
             // see it in the scene and the app wont crash. You will see an error message appear in the console.
-            asset_manager.get_mesh("example/map/map01.gltf")
+            asset_manager.get_mesh("example/meshes/cube/cube.gltf")
         };
 
         // Here we create our game entity that contains 3 components.
@@ -105,10 +105,7 @@ impl harmony::AppState for AppState {
         // 3. The transform which allows us to render the mesh using it's world cords. This also includes stuff like
         // rotation and scale.
 
-        let mut transform = Transform::new(app);
-        transform.scale = Vec3::new(0.01, 0.01, 0.01);
-        // transform.position = Vec3::new(400.0, 0.0, 0.0);
-        transform.rotate_on_x(90.0f32.to_radians());
+        let transform = Transform::new(app);
         app.current_scene
             .world
             .insert((), vec![(Mesh::new(mesh_handle), transform)]);
